e5411cdab0bb2c2df8f7f4842f57a2b9ab5c3209
Robert <r@snow87.com>
2018-03-28 00:03:52 -0600

    added more tests and fixed more bugs

--- src/test/java/com/babyorm/BabyRepoTest.java
+++ src/test/java/com/babyorm/BabyRepoTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BabyRepoTest {
@@ -59,7 +62,53 @@ class BabyRepoTest {
     }
 
     @Test
-    void getManyBy() {
+    void getManyBy_fieldName() {
+        BabyRepo<Baby> repo = new BabyRepo<Baby>(){};
+        Baby baby = new Baby();
+        baby.setHairColor("brown");
+        baby.setNumberOfToes(9);
+        baby.setName("Charlie");
+        repo.insert(baby);
+        baby.setName("bob");
+        repo.insert(baby);
+
+        List<Baby> gotten = repo.getManyBy("hairColor", "brown");
+        assertTrue(gotten.size()>1);
+        assertTrue(gotten.stream().anyMatch(b->"bob".equals(b.getName())));
+        assertTrue(gotten.stream().allMatch(b->"brown".equals(b.getHairColor())));
+    }
+
+    @Test
+    void getManyBy_collectionValue() {
+        BabyRepo<Baby> repo = new BabyRepo<Baby>(){};
+        Baby baby = new Baby();
+        baby.setHairColor("red");
+        baby.setNumberOfToes(9);
+        baby.setName("Betty");
+        repo.insert(baby);
+        baby.setHairColor("red");
+        repo.insert(baby);
+
+        List<Baby> gotten = repo.getManyBy("hairColor", Arrays.asList("brown", "red"));
+        assertTrue(gotten.size()>1);
+        assertTrue(gotten.stream().allMatch(b->"brown".equals(b.getHairColor()) || "red".equals(b.getHairColor())));
+    }
+
+    @Test
+    void getManyBy_colName() {
+        BabyRepo<Baby> repo = new BabyRepo<Baby>(){};
+        Baby baby = new Baby();
+        baby.setHairColor("brown");
+        baby.setNumberOfToes(9);
+        baby.setName("Charlie");
+        repo.insert(baby);
+        baby.setName("bob");
+        repo.insert(baby);
+
+        List<Baby> gotten = repo.getManyBy("hair_color", "brown");
+        assertTrue(gotten.size()>1);
+        assertTrue(gotten.stream().anyMatch(b->"bob".equals(b.getName())));
+        assertTrue(gotten.stream().allMatch(b->"brown".equals(b.getHairColor())));
     }
 
     @Test
