46614db114fd6f3d1424bf748f2694940e933f5a
Robert <r@snow87.com>
2018-04-03 00:48:21 -0600

    cleanup case impl

--- src/main/java/com/babyorm/util/Case.java
+++ src/main/java/com/babyorm/util/Case.java
@@ -1,23 +1,26 @@
 package com.babyorm.util;
 
 import java.util.Arrays;
-import java.util.regex.Pattern;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum Case {
-    CAMEL_CASE("looksLikeThis",""),
-    PASCAL_CASE("LooksLikeThis",""),
-    KEBAB_CASE("looks-like-this","-"),
-    SNAKE_CASE("looks_like_this","_"),
-    UPPER_KEBAB_CASE("LOOKS-LIKE-THIS","-"),
-    UPPER_SNAKE_CASE("LOOKS_LIKE_THIS","_");
+    CAMEL_CASE("looksLikeThis","", s->Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase()),
+    PASCAL_CASE("LooksLikeThis","", CAMEL_CASE.wordTransform),
+    KEBAB_CASE("looks-like-this","-", String::toLowerCase),
+    SNAKE_CASE("looks_like_this","_", String::toLowerCase),
+    UPPER_KEBAB_CASE("LOOKS-LIKE-THIS","-", String::toUpperCase),
+    UPPER_SNAKE_CASE("LOOKS_LIKE_THIS","_", String::toUpperCase),
+    CHOO_CHOO_TRAIN("looks#like#this","#", String::toLowerCase),
+    UPPER_CHOO_CHOO_TRAIN("LOOKS#LIKE#THIS","#", String::toUpperCase);
 
-    private static final Pattern WORD_START_PATTERN = Pattern.compile("(-[^-])|(_[^_])|(A-Z)");
     private String looksLikeThis, joiner;
+    private Function<String,String> wordTransform;
 
-    Case(String looksLikeThis, String joiner) {
+    Case(String looksLikeThis, String joiner, Function<String,String> wordTransform) {
         this.looksLikeThis = looksLikeThis;
         this.joiner = joiner;
+        this.wordTransform = wordTransform;
     }
 
     public String whatDoesItLookLike(){
@@ -25,31 +28,9 @@ public enum Case {
     }
 
     public static String convert(String input, Case to){
-        if(input == null){
-            return null;
-        }
-        String result = Arrays.stream(getWords(input)).map(s -> {
-            switch (to) {
-                case UPPER_KEBAB_CASE:
-                case UPPER_SNAKE_CASE:
-                    return s.toUpperCase();
-                case CAMEL_CASE:
-                case PASCAL_CASE:
-                    return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
-                default:
-                    return s.toLowerCase();
-            }
-        }).collect(Collectors.joining(to.joiner));
+        if(input == null) return null;
+        String[] words = input.matches(".*[-_#].*") ? input.split("[-_#]") : input.split("(?=[A-Z][a-z])");
+        String result = Arrays.stream(words).map(to.wordTransform).collect(Collectors.joining(to.joiner));
         return to == CAMEL_CASE ? Character.toLowerCase(result.charAt(0)) + result.substring(1) : result;
-    }
-
-    private static String[] getWords(String input){
-        if(input.contains("_")){
-            return input.split("_");
-        } else if(input.contains("-")){
-            return input.split("-");
-        } else {
-            return input.split("(?=[A-Z][a-z])");
-        }
     }
 }
